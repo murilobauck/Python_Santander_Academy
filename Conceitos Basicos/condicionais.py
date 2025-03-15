3543410f33c48f5864b6858042986478dc220e64
Murilo <murilobauck2@gmail.com>
2025-03-15 13:25:37 -0300

    conceitos basicos

---
+++ Conceitos Basicos/condicionais.py
@@ -0,0 +1,36 @@
+# if
+
+idade = 18
+
+if idade >= 18:
+   print ("Você é maior de idade.")
+
+
+# if else
+
+
+idade = 15
+
+if idade >= 18:
+   print ("Você é maior de idade.")
+
+else:
+   print ("Você é menor de idade.")
+
+
+# if elif else
+
+
+nota = 85
+
+if nota >= 90:
+   print ("Excelente")
+
+elif nota >= 80:
+   print ("Muito bom")
+
+elif nota >= 70:
+   print ("Bom")
+
+else:
+   print ("Precisa melhorar")
---
+++ Conceitos Basicos/dados.py
@@ -0,0 +1,39 @@
+#int
+
+idade = 25
+quantidade = 100
+
+#float
+
+preço = 9.99
+altura = 1.75
+
+'''
+strings
+para incluir caracteres especiais nas cadeias utiliza-se o caractere de escape \
+também pode utilizar a notação de tripla aspa (\'''...\''' ou """...""") para criar cadeias de várias linhas
+'''
+
+nome = "Juan"
+mensagem = '¡Hola, mundo!'
+
+'''
+boolean
+sempre com a primeira letra maiúscula
+'''
+
+é_maior_de_idade = True
+tem_desconto = False
+
+
+a = b = c = 10
+
+#neste caso a, b e c são variáveis que possuem o mesmo valor
+
+'''
+Os nomes das variáveis só podem conter letras, números e sublinhados (_). Não podem começar com um número.
+
+O Python diferencia maiúsculas de minúsculas, então nome e Nome são variáveis diferentes.
+
+Não se pode usar palavras-chave reservadas do Python como nomes de variáveis (por exemplo, if, else, for, while, etc.).
+'''
