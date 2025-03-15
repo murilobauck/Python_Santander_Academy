8793c18862328cbea69a65eefdd563f99b919919
Murilo <murilobauck2@gmail.com>
2025-03-15 12:42:33 -0300

    conceitos

---
+++ Conceitos Basicos/conceitos.py
@@ -0,0 +1,43 @@
+instrucao1 = 1
+instrucao2 = 2
+instrucao3 = 3
+instrucao4 = 4
+condition = True
+a = 1
+b = 2
+c = 3
+
+print ("Olá, Mundo!")
+
+# Este é um comentário de uma única linha
+
+"""
+Este é um comentário
+de várias linhas
+"""
+
+if condition:
+
+    # Bloco de código se a condição for verdadeira
+
+    instrucao1
+    instrucao2
+
+else:
+
+    # Bloco de código se a condição for falsa
+
+    instrucao3
+    instrucao4
+
+# Posso dar varias instrucoes seguidas com o uso de ;
+
+if condition:
+    instrucao1; instrucao2
+
+else:
+    instrucao3; instrucao4
+
+#Os parênteses são utilizados para agrupar expressões, definir funções e realizar chamadas a funções.
+
+resultado = (a + b) * c
